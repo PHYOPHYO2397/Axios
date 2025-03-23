a3599f2b6591a557741353b7b491b8939ba09a6d
PPMT <phyophyomyatthu3@gmail.com>
2025-03-23 19:59:03 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { IoHome } from "react-icons/io5";
+
+function App() {
+  return (
+    <>
+      <IoHome /> <p>Test</p>
+    </>
+  );
+}
+
+export default App;
